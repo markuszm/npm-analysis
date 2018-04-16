623fa772d096a7ee9159cbbfc3aef08ce43da317
Markus Zimmermann <markus_zimmermann-ffm@t-online.de>
2018-04-16 11:13:18 +0200

    Added different fileName for scoped package names and improved error logging

--- cmd/package-dl/main/main.go
+++ cmd/package-dl/main/main.go
@@ -10,6 +10,8 @@ import (
 	urlParser "net/url"
 	"path"
 	"io"
+	"strings"
+	"github.com/pkg/errors"
 )
 
 const PATH_TO_NPM_JSON = "/home/markus/npm-analysis/npm_download.json"
@@ -61,13 +63,11 @@ func worker(id int, jobs chan string, finished chan bool) {
 }
 
 func downloadPackage(url string) {
-	parsedUrl, parseErr := urlParser.Parse(url)
-
-	if parseErr != nil {
-		log.Fatal(parseErr)
+	fileName, fileNameErr := generatePackageFileName(url)
+	if fileNameErr != nil {
+		log.Fatal(errors.Wrapf(fileNameErr, "Error downloading package: %s", url))
 	}
 
-	fileName := path.Base(parsedUrl.Path)
 	filePath := path.Join(DOWNLOAD_PATH, fileName)
 	if _, err := os.Stat(filePath); err == nil {
 		// path exists already - skip
@@ -75,7 +75,7 @@ func downloadPackage(url string) {
 	}
 	resp, requestErr := http.Get(url)
 	if requestErr != nil {
-		log.Fatal(requestErr)
+		log.Fatal(errors.Wrapf(requestErr, "Error downloading package: %s", url))
 	}
 
 	defer resp.Body.Close()
@@ -83,13 +83,29 @@ func downloadPackage(url string) {
 	file, createFileErr := os.Create(filePath)
 
 	if createFileErr != nil {
-		log.Fatal("No write access to download path")
+		log.Fatal(errors.Wrapf(createFileErr, "Error downloading package: %s", url))
 	}
 
 	_, copyErr := io.Copy(file, resp.Body)
 
 	if copyErr != nil {
-		log.Fatal(copyErr)
+		log.Fatal(errors.Wrapf(copyErr, "Error downloading package: %s", url))
 	}
 
+}
+
+func generatePackageFileName(url string) (string, error) {
+	parsedUrl, parseErr := urlParser.Parse(url)
+	if parseErr != nil {
+		return "", parseErr
+	}
+	pathFragments := strings.Split(parsedUrl.Path, "/")
+	// take scoped name concatenated with file name
+	scopedName := pathFragments[1]
+	packageFileName := path.Base(parsedUrl.Path)
+	fileName := packageFileName
+	if strings.HasPrefix(scopedName, "@") {
+		fileName = scopedName + "_" + packageFileName
+	}
+	return fileName, nil
 }
