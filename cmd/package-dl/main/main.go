0bd92a0ca01080f9264565851697890bee3e13f3
Markus Zimmermann <markus_zimmermann-ffm@t-online.de>
2018-04-16 13:56:18 +0200

    refactored package download

--- cmd/package-dl/main/main.go
+++ cmd/package-dl/main/main.go
@@ -3,22 +3,16 @@ package main
 import (
 	"io/ioutil"
 	"github.com/buger/jsonparser"
-	"net/http"
 	"log"
 	"fmt"
-	"os"
-	urlParser "net/url"
-	"path"
-	"io"
-	"strings"
-	"github.com/pkg/errors"
+	"npm-analysis/downloader"
 )
 
 const PATH_TO_NPM_JSON = "/home/markus/npm-analysis/npm_download.json"
 
 const DOWNLOAD_PATH = "/media/markus/Seagate Expansion Drive/NPM"
 
-const workerNumber = 10
+const workerNumber = 5
 
 func main() {
 	data, readErr := ioutil.ReadFile(PATH_TO_NPM_JSON)
@@ -56,56 +50,9 @@ func main() {
 
 func worker(id int, jobs chan string, finished chan bool) {
 	for j := range jobs {
-		downloadPackage(j)
+		downloader.DownloadPackage(DOWNLOAD_PATH, j)
 		fmt.Println("worker", id, "finished job", j)
 	}
 	finished <- true
 }
 
-func downloadPackage(url string) {
-	fileName, fileNameErr := generatePackageFileName(url)
-	if fileNameErr != nil {
-		log.Fatal(errors.Wrapf(fileNameErr, "Error downloading package: %s", url))
-	}
-
-	filePath := path.Join(DOWNLOAD_PATH, fileName)
-	if _, err := os.Stat(filePath); err == nil {
-		// path exists already - skip
-		return
-	}
-	resp, requestErr := http.Get(url)
-	if requestErr != nil {
-		log.Fatal(errors.Wrapf(requestErr, "Error downloading package: %s", url))
-	}
-
-	defer resp.Body.Close()
-
-	file, createFileErr := os.Create(filePath)
-
-	if createFileErr != nil {
-		log.Fatal(errors.Wrapf(createFileErr, "Error downloading package: %s", url))
-	}
-
-	_, copyErr := io.Copy(file, resp.Body)
-
-	if copyErr != nil {
-		log.Fatal(errors.Wrapf(copyErr, "Error downloading package: %s", url))
-	}
-
-}
-
-func generatePackageFileName(url string) (string, error) {
-	parsedUrl, parseErr := urlParser.Parse(url)
-	if parseErr != nil {
-		return "", parseErr
-	}
-	pathFragments := strings.Split(parsedUrl.Path, "/")
-	// take scoped name concatenated with file name
-	scopedName := pathFragments[1]
-	packageFileName := path.Base(parsedUrl.Path)
-	fileName := packageFileName
-	if strings.HasPrefix(scopedName, "@") {
-		fileName = scopedName + "_" + packageFileName
-	}
-	return fileName, nil
-}
---
+++ downloader/pkgdownloader.go
@@ -0,0 +1,61 @@
+package downloader
+
+import (
+	"log"
+	"github.com/pkg/errors"
+	"path"
+	"os"
+	"net/http"
+	"io"
+	"strings"
+	"net/url"
+)
+
+func DownloadPackage(downloadPath, url string) {
+	fileName, fileNameErr := GeneratePackageFileName(url)
+	if fileNameErr != nil {
+		log.Fatal(errors.Wrapf(fileNameErr, "Error downloading package: %s", url))
+	}
+
+	filePath := path.Join(downloadPath, fileName)
+	if _, err := os.Stat(filePath); err == nil {
+		// path exists already - skip
+		return
+	}
+	resp, requestErr := http.Get(url)
+	if requestErr != nil {
+		log.Fatal(errors.Wrapf(requestErr, "Error downloading package: %s", url))
+	}
+
+	defer resp.Body.Close()
+
+	file, createFileErr := os.Create(filePath)
+
+	if createFileErr != nil {
+		log.Fatal(errors.Wrapf(createFileErr, "Error downloading package: %s", url))
+	}
+
+	_, copyErr := io.Copy(file, resp.Body)
+
+	if copyErr != nil {
+		log.Fatal(errors.Wrapf(copyErr, "Error downloading package: %s", url))
+	}
+
+}
+
+func GeneratePackageFileName(downloadUrl string) (string, error) {
+	parsedUrl, parseErr := url.Parse(downloadUrl)
+	if parseErr != nil {
+		return "", parseErr
+	}
+	pathFragments := strings.Split(parsedUrl.Path, "/")
+	// take scoped name concatenated with file name
+	scopedName := pathFragments[1]
+	packageFileName := path.Base(parsedUrl.Path)
+	fileName := packageFileName
+	if strings.HasPrefix(scopedName, "@") {
+		fileName = scopedName + "_" + packageFileName
+	}
+	return fileName, nil
+}
+
