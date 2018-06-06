98d8acd72d47c24adb7eacb21643e69ff1074c14
Markus Zimmermann <markus_zimmermann-ffm@t-online.de>
2018-06-07 00:22:57 +0200

    fixed exceeding of local socket connections

--- database/mysql.go
+++ database/mysql.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/pkg/errors"
+	"time"
 )
 
 type Mysql struct{}
@@ -11,7 +12,8 @@ type Mysql struct{}
 func (d *Mysql) InitDB(dataSource string) (*sql.DB, error) {
 	db, openError := sql.Open("mysql", dataSource)
 	db.SetMaxOpenConns(100)
-	db.SetConnMaxLifetime(-1)
+	db.SetConnMaxLifetime(30 * time.Second)
+	db.SetMaxIdleConns(100)
 	if openError != nil {
 		return nil, errors.Wrap(openError, "Error opening database")
 	}
