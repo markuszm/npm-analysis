563f069761e850498b85804d0f874491ef8ca0b3
Markus Zimmermann <markus_zimmermann-ffm@t-online.de>
2018-07-03 17:45:35 +0200

    exports analysis: added babel parser skeleton and some simple detection of method name

--- codeanalysispipeline/codeanalysis/testfiles/exportcjstest/indirectExport.js
+++ codeanalysispipeline/codeanalysis/testfiles/exportcjstest/indirectExport.js
@@ -1,3 +1,11 @@
 exports = module.exports;
 
 exports.divide = (a,b) => a/b;
+
+exports = {
+    add2(a) { return a+2 },
+    add4(a) { return a+4 },
+    add8(a) { return a+8 },
+    add16(a) { return a+16 },
+    add32(a) { return a+32 },
+};
