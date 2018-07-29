83e6b70337f0842944aa3400ddbe85561158fba7
Markus Zimmermann <markus_zimmermann-ffm@t-online.de>
2018-07-29 20:13:36 +0200

    rebuild binaries and added test for reassignments in export analysis

---
+++ codeanalysis/testfiles/export/scoping/reassignment.js
@@ -0,0 +1,20 @@
+var condition = false;
+class OAuth {
+  constructor(a) {}
+
+  foo(a) {}
+}
+
+var foo = 20;
+
+if (condition) {
+    foo = new OAuth("b");
+} else {
+  foo = {
+    foo(a) {},
+    bar(a) {},
+    foobar(a) {}
+  };
+}
+
+module.exports.foo = foo;
