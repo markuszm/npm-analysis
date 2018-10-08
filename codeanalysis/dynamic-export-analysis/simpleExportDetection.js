5cd0f3b5f885013d0f27054f36f9b4cd03dc1422
Markus Zimmermann <markuszimmermann92@gmail.com>
2018-10-08 18:42:03 +0200

    dynamic export analysis: added detection of  class members and default function

--- codeanalysis/dynamic-export-analysis/simpleExportDetection.js
+++ codeanalysis/dynamic-export-analysis/simpleExportDetection.js
@@ -18,14 +18,28 @@ let exportedFunctions = [];
 // requiring package and retrieving function definitions
 const imported = require(packageName);
 
-const props = Object.getOwnPropertyNames(imported);
+let props = Object.getOwnPropertyNames(imported);
+// also tracks functions on class instance by getting the prototype of the object see https://stackoverflow.com/questions/31054910/get-functions-methods-of-a-class
+if (typeof imported === "object") {
+    props.append(...Object.getOwnPropertyNames(Object.getPrototypeOf(imported)))
+}
+if(typeof imported === "function") {
+    const functionBody = imported[prop].toString();
+    exportedFunctions.push({
+        Name: "default",
+        InternalName: imported.name,
+        Contents: functionBody
+    });
+}
+// TODO: find location via searching files
 for (let prop of props) {
     try {
         if (typeof imported[prop] === "function") {
+            const functionBody = imported[prop].toString();
             exportedFunctions.push({
                 Name: prop,
                 InternalName: imported[prop].name,
-                Contents: Buffer.from(imported[prop].toString()).toString('base64')
+                Contents: functionBody
             });
         }
     } catch (error) {}
