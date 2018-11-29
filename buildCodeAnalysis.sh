d23ec1bd00eacab9685837cdbe9325c03700bd81
Markus Zimmermann <markuszimmermann92@gmail.com>
2018-11-29 11:59:51 +0100

    fix vulnerability in js code analyses

--- buildCodeAnalysis.sh
+++ buildCodeAnalysis.sh
@@ -2,12 +2,14 @@
 
 rm -rf bin
 
+mkdir bin
+
 # Build pipeline executor (GO)
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -v -installsuffix cgo -o bin/pipeline ./cmd/code-analysis/main/main.go
 
 # Build callgraph analysis (Node.js)
 cd codeanalysis/js/callgraph-analysis/
-npm i
+npm ci
 npm run pack
 cp ./analysis ../../../bin/callgraph-analysis
 
@@ -15,7 +17,7 @@ cd ../../../
 
 # Build export analysis (Node.js)
 cd codeanalysis/js/exports-analysis
-npm i
+npm ci
 npm run pack
 cp ./analysis ../../../bin/exports-analysis
 
@@ -23,7 +25,7 @@ cd ../../../
 
 # Build import analysis (Node.js)
 cd codeanalysis/js/import-analysis
-npm i
+npm ci
 npm run pack
 cp ./analysis ../../../bin/import-analysis
 
