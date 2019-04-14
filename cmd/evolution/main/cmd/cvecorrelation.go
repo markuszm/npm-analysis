aa1283e91252c84b4bb001c53db7b194b38a402f
Markus Zimmermann <markuszimmermann92@gmail.com>
2019-04-14 21:32:38 +0200

    added cve correlation calculation

---
+++ cmd/evolution/main/cmd/cvecorrelation.go
@@ -0,0 +1,93 @@
+package cmd
+
+import (
+	"encoding/json"
+	reach "github.com/markuszm/npm-analysis/evolution/maintainerreach"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"os"
+	"path"
+)
+
+const cveCorrelationLatestDependenciesJsonPath = "./db-data/latestDependenciesTimeline.json"
+
+var cveCorrelationCVEInput string
+
+var cveCorrelationOutputFolder string
+
+var cveCorrelationLatestDependencies map[string]map[string]bool
+
+// calculates Package reach of a packages and plots it
+var cveCorrelationCmd = &cobra.Command{
+	Use:   "cveCorrelate",
+	Short: "calculates correlation between vulnerable and transitive dependencies",
+	Long:  `...`,
+	Run: func(cmd *cobra.Command, args []string) {
+		initializeLogger()
+
+		cveCorrelationLatestDependencies = reach.LoadJSONLatestDependencies(cveCorrelationLatestDependenciesJsonPath)
+		calculateCorrelation()
+	},
+}
+
+func calculateCorrelation() {
+	file, err := ioutil.ReadFile(cveCorrelationCVEInput)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	var cvePackageList []string
+	err = json.Unmarshal(file, &cvePackageList)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	var cvePackageSet = make(map[string]bool, 0)
+
+	for _, p := range cvePackageList {
+		cvePackageSet[p] = true
+	}
+
+	var results []CveCorrelationResult
+
+	for p, deps := range cveCorrelationLatestDependencies {
+		result := CveCorrelationResult{
+			PackageName:  p,
+			PackageCost:  len(deps),
+			IsVulnerable: false,
+		}
+		for d, ok := range deps {
+			if ok && cvePackageSet[d] {
+				result.IsVulnerable = true
+			}
+		}
+
+		results = append(results, result)
+		logger.Debugf("Finished %s", p)
+	}
+
+	bytes, err := json.Marshal(results)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	resultFilePath := path.Join(cveCorrelationOutputFolder, "cveCorrelation.json")
+	err = ioutil.WriteFile(resultFilePath, bytes, os.ModePerm)
+	if err != nil {
+		logger.Fatal(err)
+	}
+}
+
+type CveCorrelationResult struct {
+	PackageName  string
+	IsVulnerable bool
+	PackageCost  int
+}
+
+func init() {
+	rootCmd.AddCommand(cveCorrelationCmd)
+
+	cveCorrelationCmd.Flags().StringVar(&cveCorrelationCVEInput, "cveInput", "/home/markus/node-vulns/cvePackages.json", "input file containing packages that are vulnerable")
+	cveCorrelationCmd.Flags().StringVar(&cveCorrelationOutputFolder, "output", "/home/markus/npm-analysis/", "output folder for results")
+
+}
